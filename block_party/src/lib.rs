e76dc84deee294f998386980642ba6579084c136
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2024-05-27 12:52:48 +0200

    fix wasm file paths

--- block_party/src/lib.rs
+++ block_party/src/lib.rs
@@ -8,19 +8,19 @@ use std::str::FromStr;
 use namada_proof_of_stake::storage::{read_pos_params, write_pos_params};
 
 const TX_UPDATE_ACCOUNT_NAME: &str = "tx_update_account.wasm";
-const TX_UPDATE_ACCOUNT_BYTES: &[u8] = include_bytes!("../../wasms/tx_update_steward_commission.1b4036f3d55505cc22ca0955f9bed199d9173566130f8e59143d75bcac92e165.wasm");
+const TX_UPDATE_ACCOUNT_BYTES: &[u8] = include_bytes!("../../wasms/tx_update_account.04256c4087de46e71e6f323b696ec412e41cfd3ec337010c1f2a590037c0cf8c.wasm");
 
 const TX_INIT_ACCOUNT_NAME: &str = "tx_init_account.wasm";
-const TX_INIT_ACCOUNT_BYTES: &[u8] = include_bytes!("../../wasms/tx_update_steward_commission.1b4036f3d55505cc22ca0955f9bed199d9173566130f8e59143d75bcac92e165.wasm");
+const TX_INIT_ACCOUNT_BYTES: &[u8] = include_bytes!("../../wasms/tx_init_account.4078c07a356691b23049055df6fc7fb99c32b75f488f435538c1a960cdb41153.wasm");
 
 const TX_CLAIM_REWARDS_NAME: &str = "tx_claim_rewards.wasm";
-const TX_CLAIM_REWARDS_BYTES: &[u8] = include_bytes!("../../wasms/tx_update_steward_commission.1b4036f3d55505cc22ca0955f9bed199d9173566130f8e59143d75bcac92e165.wasm");
+const TX_CLAIM_REWARDS_BYTES: &[u8] = include_bytes!("../../wasms/tx_claim_rewards.af4cd35b30f17cf2440be74bedeb74296aac21e4ab1ea7143f3a3dede722a0ed.wasm");
 
 const TX_UPDATE_STEWARD_COMMISSION_NAME: &str = "tx_update_steward_commission.wasm";
-const TX_UPDATE_STEWARD_COMMISSION_BYTES: &[u8] = include_bytes!("../../wasms/tx_update_steward_commission.1b4036f3d55505cc22ca0955f9bed199d9173566130f8e59143d75bcac92e165.wasm");
+const TX_UPDATE_STEWARD_COMMISSION_BYTES: &[u8] = include_bytes!("../../wasms/tx_update_steward_commission.01e2395fab96f0ac8251329370149144159d378abcf5a414df1c030a329bda82.wasm");
 
 const TX_RESIGN_STEWARD_NAME: &str = "tx_resign_steward.wasm";
-const TX_RESIGN_STEWARD_BYTES: &[u8] = include_bytes!("../../wasms/tx_update_steward_commission.1b4036f3d55505cc22ca0955f9bed199d9173566130f8e59143d75bcac92e165.wasm");
+const TX_RESIGN_STEWARD_BYTES: &[u8] = include_bytes!("../../wasms/tx_resign_steward.b5dbddb91e2627d400fa6ca83970a8396dd968ab25ff896b845b19a813729e04.wasm");
 
 #[transaction(gas = 10000)]
 fn apply_tx(ctx: &mut Ctx, _tx_data: BatchedTx) -> TxResult {
