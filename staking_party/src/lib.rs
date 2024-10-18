467bca05ae98baf8dc439eb1832ef336c8d80740
brentstone <brentstone01@gmail.com>
2024-10-17 22:09:02 -0400

    resolve after rebase

--- staking_party/src/lib.rs
+++ staking_party/src/lib.rs
@@ -10,19 +10,19 @@ fn apply_tx(ctx: &mut Ctx, _tx_data: BatchedTx) -> TxResult {
     // PoS inflation
     let mut pos_params = read_pos_params::<Ctx, governance::Store<Ctx>>(ctx)?.owned;
     pos_params.max_inflation_rate = Dec::from_str("0.1").unwrap();
-    pos_params.target_staked_ratio = Dec::from_str("0.666667").unwrap();
-    pos_params.rewards_gain_p = Dec::from_str("2.5").unwrap();
-    pos_params.rewards_gain_d = Dec::from_str("2.5").unwrap();
+    pos_params.target_staked_ratio = Dec::from_str("0.4").unwrap();
+    pos_params.rewards_gain_p = Dec::from_str("0.25").unwrap();
+    pos_params.rewards_gain_d = Dec::from_str("0.25").unwrap();
     write_pos_params(ctx, &pos_params)?;
 
     // PGF inflation
     let pgf_inflation_key = governance::pgf::storage::keys::get_pgf_inflation_rate_key();
-    let pgf_inflation_rate = Dec::from_str("0.025").unwrap();
+    let pgf_inflation_rate = Dec::from_str("0.05").unwrap();
     ctx.write(&pgf_inflation_key, pgf_inflation_rate)?;
 
     // PGF stewards inflation
     let steward_inflation_key = governance::pgf::storage::keys::get_steward_inflation_rate_key();
-    let steward_inflation_rate = Dec::from_str("0.001").unwrap();
+    let steward_inflation_rate = Dec::from_str("0.005").unwrap();
     ctx.write(&steward_inflation_key, steward_inflation_rate)?;
 
     Ok(())
