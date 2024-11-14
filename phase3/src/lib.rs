abd8ed2ff6cd295acacc31704d6f7c137128a0a7
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2024-11-14 14:56:53 +0100

    test

--- phase3/src/lib.rs
+++ phase3/src/lib.rs
@@ -1,16 +1,18 @@
 use namada_tx_prelude::*;
-use parameters_storage::{get_gas_scale_key, get_masp_fee_payment_gas_limit_key, get_max_block_gas_key};
+use parameters_storage::{
+    get_gas_scale_key, get_masp_fee_payment_gas_limit_key, get_max_block_gas_key,
+};
 
 #[transaction]
 fn apply_tx(ctx: &mut Ctx, _tx_data: BatchedTx) -> TxResult {
     let gas_scale_key = get_gas_scale_key();
-    ctx.write(&gas_scale_key, 50_000 as u64)?;
+    ctx.write(&gas_scale_key, 50_000_u64)?;
 
     let masp_fee_payment_gas_limit_key = get_masp_fee_payment_gas_limit_key();
-    ctx.write(&masp_fee_payment_gas_limit_key, 80_000 as u64)?;
+    ctx.write(&masp_fee_payment_gas_limit_key, 80_000_u64)?;
 
     let max_block_gas_key = get_max_block_gas_key();
-    ctx.write(&max_block_gas_key, 3_000_000 as u64)?;
+    ctx.write(&max_block_gas_key, 3_000_000_u64)?;
 
     Ok(())
 }
