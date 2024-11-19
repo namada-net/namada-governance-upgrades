a1dfd58196530458ab0d01279b6b76a8021fe391
brentstone <brentstone01@gmail.com>
2024-11-18 16:48:30 -0800

    just enable NAM transfers in Phase 5

--- phase5/src/lib.rs
+++ phase5/src/lib.rs
@@ -9,12 +9,5 @@ fn apply_tx(ctx: &mut Ctx, _tx_data: BatchedTx) -> TxResult {
     let native_token_transferable_key = parameters_storage::get_native_token_transferable_key();
     ctx.write(&native_token_transferable_key, true)?;
 
-    // 2. Update governance parameters
-    let min_proposal_grace_epochs_key = gov_storage::keys::get_min_proposal_grace_epochs_key();
-    ctx.write(&min_proposal_grace_epochs_key, MIN_PROPOSAL_GRACE_EPOCHS)?;
-
-    let min_proposal_voting_period_key = gov_storage::keys::get_min_proposal_voting_period_key();
-    ctx.write(&min_proposal_voting_period_key, MIN_PROPOSAL_VOTING_PERIOD)?;
-
     Ok(())
 }
