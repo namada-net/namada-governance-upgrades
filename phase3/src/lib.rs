c90995aede7bbab8e48682970ecb501b5a3678ba
brentstone <brentstone01@gmail.com>
2025-01-02 17:32:35 -0800

    update with initial proposed values

--- phase3/src/lib.rs
+++ phase3/src/lib.rs
@@ -19,55 +19,48 @@ const IBC_TOKENS: [(
     MintTokenLimit,
     ThroughtputTokenLimit,
     MinimumGasPrice,
-); 7] = [
+); 6] = [
     (
-        "channel-X",
+        "channel-?",
         "uosmo",
-        MintTokenLimit::from_u64(10000000000), // 10_000 OSMO supply limit
-        ThroughtputTokenLimit::from_u64(10000000000),
-        Some(Gas::from_u64(1)), // 1 uosmo / gas unit
+        MintTokenLimit::from_u64(10752692000000),       // 10,752,692 OSMO
+        ThroughtputTokenLimit::from_u64(2150539000000), // 2,150,539 OSMO
+        Some(Gas::from_u64(10)),                        // 10 uosmo / gas unit
     ),
     (
-        "channel-Y",
+        "channel-?",
         "uatom",
-        MintTokenLimit::from_u64(10000000000), // 10_000 ATOM supply limit
-        ThroughtputTokenLimit::from_u64(10000000000),
-        Some(Gas::from_u64(1)), // 1 uatom / gas unit;
+        MintTokenLimit::from_u64(759878000000),         // 759,878 ATOM
+        ThroughtputTokenLimit::from_u64(151976000000),  // 151,976 ATOM
+        Some(Gas::from_u64(1)),                         // 1 uatom / gas unit;
     ),
     (
-        "channel-Z",
+        "channel-?",
         "utia",
-        MintTokenLimit::from_u64(10000000000), // 10_000 TIA supply limit
-        ThroughtputTokenLimit::from_u64(10000000000),
-        Some(Gas::from_u64(1)), // 1 utia / gas unit;
+        MintTokenLimit::from_u64(1018330000000),        // 1,018,330 TIA
+        ThroughtputTokenLimit::from_u64(203666000000),  // 203,666 TIA
+        Some(Gas::from_u64(1)),                         // 1 utia / gas unit;
     ),
     (
-        "channel-X",
+        "channel-?",
         "ustosmo",
-        MintTokenLimit::from_u64(10000000000), // 10_000 stOSMO supply limit
-        ThroughtputTokenLimit::from_u64(10000000000),
-        Some(Gas::from_u64(1)), // 1 ustosmo / gas unit
+        MintTokenLimit::from_u64(8196721000000),        // 8,196,721 stOSMO
+        ThroughtputTokenLimit::from_u64(1639344000000), // 1,639,344 stOSMO
+        Some(Gas::from_u64(10)),                        // 10 stuosmo / gas unit
     ),
     (
-        "channel-Y",
+        "channel-?",
         "ustatom",
-        MintTokenLimit::from_u64(10000000000), // 10_000 stATOM supply limit
-        ThroughtputTokenLimit::from_u64(10000000000),
-        Some(Gas::from_u64(1)), // 1 ustatom / gas unit;
+        MintTokenLimit::from_u64(512821000000),         // 512,821 stATOM
+        ThroughtputTokenLimit::from_u64(102564000000),  // 102,564 stATOM
+        Some(Gas::from_u64(1)),                         // 1 stuatom / gas unit;
     ),
     (
-        "channel-Z",
+        "channel-?",
         "usttia",
-        MintTokenLimit::from_u64(10000000000), // 10_000 stTIA supply limit
-        ThroughtputTokenLimit::from_u64(10000000000),
-        Some(Gas::from_u64(1)), // 1 usttia / gas unit;
-    ),
-    (
-        "channel-Z",
-        "ainj??",
-        MintTokenLimit::from_u64(10000000000), // 10e-8 INJ supply limit
-        ThroughtputTokenLimit::from_u64(10000000000),
-        Some(Gas::from_u64(2500000000000000)), // 0.0025 (2.5e15) inj (ainj) / gas unit;
+        MintTokenLimit::from_u64(946970000000),         // 946,970 stTIA
+        ThroughtputTokenLimit::from_u64(189394000000),  // 189,394 stTIA
+        Some(Gas::from_u64(1)),                         // 1 stutia / gas unit;
     ),
 ];
 
