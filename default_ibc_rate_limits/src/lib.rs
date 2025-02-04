09a3eaca1b51e34cac3cbcd8497bfcef02a578c1
brentstone <brentstone01@gmail.com>
2025-02-03 19:43:01 -0800

    default rate limits for IBC tokens to 10^8 raw tokens

---
+++ default_ibc_rate_limits/src/lib.rs
@@ -0,0 +1,28 @@
+use namada_ibc::parameters::IbcParameters;
+use namada_ibc::storage::params_key;
+use namada_tx_prelude::*;
+
+pub type ChannelId = &'static str;
+pub type BaseToken = &'static str;
+
+pub type MintTokenLimit = token::Amount;
+pub type ThroughtputTokenLimit = token::Amount;
+pub type CanBeUsedAsGas = bool;
+pub type Gas = token::Amount;
+pub type MinimumGasPrice = Option<Gas>;
+
+const MINT_LIMIT: MintTokenLimit = MintTokenLimit::from_u64(100000000);
+const THROUGHPUT_LIMIT: ThroughtputTokenLimit = ThroughtputTokenLimit::from_u64(100000000);
+
+#[transaction]
+fn apply_tx(ctx: &mut Ctx, _tx_data: BatchedTx) -> TxResult {
+    let ibc_params_key = params_key();
+
+    let ibc_params = IbcParameters {
+        default_mint_limit: MINT_LIMIT,
+        default_per_epoch_throughput_limit: THROUGHPUT_LIMIT,
+    };
+    ctx.write(&ibc_params_key, ibc_params)?;
+
+    Ok(())
+}
