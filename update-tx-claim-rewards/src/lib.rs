f686b878a187b0e826064e328e2bfb3c49b740ff
brentstone <brentstone01@gmail.com>
2025-02-17 13:43:18 -0800

    use current wasm hash on campfire

--- update-tx-claim-rewards/src/lib.rs
+++ update-tx-claim-rewards/src/lib.rs
@@ -46,7 +46,7 @@ fn apply_tx(ctx: &mut Ctx, _tx_data: BatchedTx) -> TxResult {
 }
 
 const TX_NAME: &str = "tx_claim_rewards.wasm";
-const OLD_TX_HASH: &str = "b6a1f7e069360650d2c6a1bdd2e5f4e18bb748d35dad02c31c027673fa042d8c";
+const OLD_TX_HASH: &str = "b1224953556ae9e4f83bd2c9593cfbb04a08b5592d150bff2c158225f2dbbdd6";
 const NEW_TX_HASH: &str = "b74104949ac0c35ee922fdc3f3db454627742e2483d79550c12fcf31755c6d01";
 const NEW_TX_CODE: &[u8] = include_bytes!(
     "tx_claim_rewards.b74104949ac0c35ee922fdc3f3db454627742e2483d79550c12fcf31755c6d01.wasm"
