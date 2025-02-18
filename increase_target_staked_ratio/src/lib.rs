dd0a0e40ac9402f09d237be50d48c4b2b4071a0e
brentstone <brentstone01@gmail.com>
2025-02-18 11:42:29 -0800

    make target staked ratio 100%

--- increase_target_staked_ratio/src/lib.rs
+++ increase_target_staked_ratio/src/lib.rs
@@ -8,7 +8,7 @@ use namada_proof_of_stake::storage::{read_pos_params, write_pos_params};
 #[transaction]
 fn apply_tx(ctx: &mut Ctx, _tx_data: BatchedTx) -> TxResult {
     let mut pos_params = read_pos_params::<Ctx, governance::Store<Ctx>>(ctx)?.owned;
-    pos_params.target_staked_ratio = Dec::from_str("0.55").unwrap();
+    pos_params.target_staked_ratio = Dec::from_str("1").unwrap();
     write_pos_params(ctx, &pos_params)?;
 
     Ok(())
