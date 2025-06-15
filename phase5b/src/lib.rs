f7ea7a2542fd7c13374563542e121bbe1a72cbc7
brentstone <brentstone01@gmail.com>
2025-06-15 10:19:54 -0700

    only nam rate limits

--- phase5b/src/lib.rs
+++ phase5b/src/lib.rs
@@ -10,9 +10,6 @@ const MINT_LIMIT: MintTokenLimit = MintTokenLimit::from_u128(100_000_000_000_000
 const THROUGHPUT_LIMIT: ThroughtputTokenLimit =
     ThroughtputTokenLimit::from_u128(10_000_000_000_000); // Ex: 10M NAM -> Need to update with final number
 
-pub const MIN_PROPOSAL_GRACE_EPOCHS: u64 = 8;
-pub const MIN_PROPOSAL_VOTING_PERIOD: u64 = 28;
-
 #[transaction]
 fn apply_tx(ctx: &mut Ctx, _tx_data: BatchedTx) -> TxResult {
     // 1. Enable IBC rate limits for native token
@@ -22,13 +19,6 @@ fn apply_tx(ctx: &mut Ctx, _tx_data: BatchedTx) -> TxResult {
 
     ctx.write(&mint_limit_token_key, MINT_LIMIT)?;
     ctx.write(&throughput_limit_token_key, THROUGHPUT_LIMIT)?;
-
-    // 2. Update governance parameters
-    let min_proposal_grace_epochs_key = gov_storage::keys::get_min_proposal_grace_epochs_key();
-    let min_proposal_voting_period_key = gov_storage::keys::get_min_proposal_voting_period_key();
-
-    ctx.write(&min_proposal_grace_epochs_key, MIN_PROPOSAL_GRACE_EPOCHS)?;
-    ctx.write(&min_proposal_voting_period_key, MIN_PROPOSAL_VOTING_PERIOD)?;
 
     Ok(())
 }
