612e3b2bb0fdae986fbc3e98273b14aaf19a57bc
brentstone <brentstone01@gmail.com>
2025-06-19 22:22:08 -0700

    increase max proposal code size

--- increase_target_staked_ratio/src/lib.rs
+++ increase_target_staked_ratio/src/lib.rs
@@ -1,22 +1,10 @@
-use namada_tx_prelude::{
-    gov_storage::keys::get_min_proposal_voting_period_key,
-    parameters::ProposalBytes,
-    parameters_storage::{get_max_block_gas_key, get_max_proposal_bytes_key},
-    *,
-};
+use namada_tx_prelude::{gov_storage::keys::get_max_proposal_code_size_key, *};
 
 #[transaction]
 fn apply_tx(ctx: &mut Ctx, _tx_data: BatchedTx) -> TxResult {
-    let max_proposal_bytes_key = get_max_proposal_bytes_key();
-    let min_voting_period_key = get_min_proposal_voting_period_key();
-    let max_block_gas_key = get_max_block_gas_key();
+    let max_proposal_code_size_key = get_max_proposal_code_size_key();
 
-    ctx.write(
-        &max_proposal_bytes_key,
-        ProposalBytes::new(2_000_000).unwrap(),
-    )?;
-    ctx.write(&min_voting_period_key, 2_u64)?;
-    ctx.write(&max_block_gas_key, 10_000_000_u64)?;
+    ctx.write(&max_proposal_code_size_key, 2_000_000_u64)?;
 
     Ok(())
 }
