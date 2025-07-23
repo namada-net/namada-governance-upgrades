1d2991f8ef0b2d167c799fbbec1a0dfa451c906a
iskay <ikay@lakeheadu.ca>
2025-07-23 09:47:55 -0400

    add wasm to reduce staking max inflation rate and target locked

---
+++ reduce_staking_rewards/src/lib.rs
@@ -0,0 +1,16 @@
+use dec::Dec;
+use namada_tx_prelude::*;
+
+use std::str::FromStr;
+
+use namada_proof_of_stake::storage::{read_pos_params, write_pos_params};
+
+#[transaction]
+fn apply_tx(ctx: &mut Ctx, _tx_data: BatchedTx) -> TxResult {
+    let mut pos_params = read_pos_params::<Ctx, governance::Store<Ctx>>(ctx)?.owned;
+    pos_params.max_inflation_rate = Dec::from_str("0.025").unwrap();
+    pos_params.target_staked_ratio = Dec::from_str("0.40").unwrap();
+    write_pos_params(ctx, &pos_params)?;
+
+    Ok(())
+}
