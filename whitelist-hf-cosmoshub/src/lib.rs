72cea5aa082d0657c680b5744bb8871463de6a97
iskay <ikay@lakeheadu.ca>
2025-08-22 07:54:10 -0400

    add proposal for new cosmoshub channel-26

---
+++ whitelist-hf-cosmoshub/src/lib.rs
@@ -0,0 +1,19 @@
+use namada_tx_prelude::*;
+
+// Channel ID(s) where the transfers are unlimited
+const CHANNEL_IDS: [&str; 4] = [
+    "channel-26", // new cosmoshub-4 channel (08/22/2025)
+];
+
+#[transaction]
+fn apply_tx(ctx: &mut Ctx, _tx_data: BatchedTx) -> TxResult {
+    for channel_id in CHANNEL_IDS {
+        let unlimited_channel_id = channel_id.parse().unwrap();
+        let unlimited_channel_key =
+            ibc::unlimited_channel_key(&unlimited_channel_id);
+    
+        ctx.write(&unlimited_channel_key, ())?;
+    }
+
+    Ok(())
+}
