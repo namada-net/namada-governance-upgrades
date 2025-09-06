5fad42152f631607a7763352f230040629f7d3e4
brentstone <brentstone01@gmail.com>
2025-09-06 10:55:18 +0200

    update target amounts and gains

--- phase4/src/lib.rs
+++ phase4/src/lib.rs
@@ -1,7 +1,6 @@
 use dec::Dec;
 use namada_tx_prelude::*;
 use std::str::FromStr;
-use token::storage_key::balance_key;
 
 pub type Denomination = u8;
 pub type ChannelId = &'static str;
@@ -16,7 +15,6 @@ const IBC_TOKENS: [(
     Denomination,
     ChannelId,
     BaseToken,
-    TokenMaxReward,
     TokenTargetLockedAmount,
     KpGain,
     KdGain,
@@ -25,26 +23,23 @@ const IBC_TOKENS: [(
         0,
         "channel-1",
         "uosmo",
-        "0.009",
-        13_300_000_000_000, // 13.3m OSMO
-        "50",
-        "85",
+        1_500_000_000_000, // 1.5M OSMO
+        "4.74",
+        "8.05",
     ),
     (
         0,
         "channel-2",
         "uatom",
-        "0.009",
-        500_000_000_000, // 500k ATOM
-        "50",
-        "85",
+        125_000_000_000, // 125k ATOM
+        "126",
+        "214",
     ),
     (
         0,
         "channel-3",
         "utia",
-        "0.009",
-        1_260_000_000_000, // 1.26 TIA
+        110_000_000_000, // 110K TIA
         "50",
         "85",
     ),
@@ -52,86 +47,49 @@ const IBC_TOKENS: [(
         0,
         "channel-0",
         "stuosmo",
-        "0.009",
-        1_000_000_000_000, // 1m stOSMO
-        "50",
-        "85",
+        700_000_000_000, // 700k stOSMO
+        "63",
+        "107",
     ),
     (
         0,
         "channel-0",
         "stuatom",
-        "0.009",
-        31_000_000_000, // 31k stATOM
-        "50",
-        "85",
+        15_000_000_000, // 15k stATOM
+        "2032",
+        "3455",
     ),
     (
         0,
         "channel-0",
         "stutia",
-        "0.009",
-        112_000_000_000, // 112k stTIA
-        "50",
-        "85",
+        50_000_000_000, // 50k stTIA
+        "563",
+        "956",
     ),
     (
         0,
         "channel-5",
         "uusdc",
-        "0.018",
-        2_200_000_000_000, // 2.2M USDC
-        "50",
-        "85",
+        1_100_000_000_000, // 1.1M USDC
+        "29",
+        "49",
     ),
 ];
 
 #[transaction]
 fn apply_tx(ctx: &mut Ctx, _tx_data: BatchedTx) -> TxResult {
-    // Read the current MASP token map
-    // NOTE: Not needed for the exact mainnet Phase 4 since this logic was included in the Phase 3 proposal
-    // let token_map_key = token::storage_key::masp_token_map_key();
-    // let mut token_map = ctx
-    //     .read::<masp::TokenMap>(&token_map_key)?
-    //     .unwrap_or_default();
-
     // Enable shielded set rewards for ibc tokens
-    for (denomination, channel_id, base_token, max_reward, target_locked_amount, kp, kd) in
-        IBC_TOKENS
-    {
+    for (denomination, channel_id, base_token, target_locked_amount, kp, kd) in IBC_TOKENS {
         let ibc_denom = format!("transfer/{channel_id}/{base_token}");
         let token_address = ibc::ibc_token(&ibc_denom);
 
-        let shielded_token_last_inflation_key =
-            token::storage_key::masp_last_inflation_key(&token_address);
-        let shielded_token_last_locked_amount_key =
-            token::storage_key::masp_last_locked_amount_key(&token_address);
-        let shielded_token_max_rewards_key =
-            token::storage_key::masp_max_reward_rate_key(&token_address);
         let shielded_token_target_locked_amount_key =
             token::storage_key::masp_locked_amount_target_key(&token_address);
         let shielded_token_kp_gain_key = token::storage_key::masp_kp_gain_key(&token_address);
         let shielded_token_kd_gain_key = token::storage_key::masp_kd_gain_key(&token_address);
 
-        // Add the ibc token to the masp token map
-        // NOTE: Not needed for the exact mainnet Phase 4 since this logic was included in the Phase 3 proposal
-        // token_map.insert(ibc_denom, token_address.clone());
-
-        // Read the current balance of the IBC token in MASP and set that as initial locked amount
-        let ibc_balance_key = balance_key(
-            &token_address,
-            &Address::Internal(address::InternalAddress::Masp),
-        );
-        let current_ibc_amount = ctx.read::<token::Amount>(&ibc_balance_key)?.unwrap();
-        ctx.write(&shielded_token_last_locked_amount_key, current_ibc_amount)?;
-
-        // Initialize the remaining MASP inflation keys
-        ctx.write(&shielded_token_last_inflation_key, token::Amount::zero())?;
-
-        ctx.write(
-            &shielded_token_max_rewards_key,
-            Dec::from_str(max_reward).unwrap(),
-        )?;
+        // Write new MASP inflation param values
         ctx.write(
             &shielded_token_target_locked_amount_key,
             token::Amount::from_uint(target_locked_amount, denomination).unwrap(),
@@ -139,10 +97,6 @@ fn apply_tx(ctx: &mut Ctx, _tx_data: BatchedTx) -> TxResult {
         ctx.write(&shielded_token_kp_gain_key, Dec::from_str(kp).unwrap())?;
         ctx.write(&shielded_token_kd_gain_key, Dec::from_str(kd).unwrap())?;
     }
-
-    // Write the token map back to storage
-    // NOTE: Not needed for the exact mainnet Phase 4 since this logic was included in the Phase 3 proposal
-    // ctx.write(&token_map_key, token_map)?;
 
     Ok(())
 }
