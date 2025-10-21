825212da448aebe27a4f9f27c75a5ce07521f160
Mateusz Jasiuk <mateusz.jasiuk@gmail.com>
2025-10-21 14:06:50 +0200

    feat: allow new atom as gas token in hf

---
+++ hf-atoms-gas-token/src/lib.rs
@@ -0,0 +1,40 @@
+use std::collections::BTreeMap;
+
+use namada_tx_prelude::*;
+use parameters_storage::get_gas_cost_key;
+
+pub type ChannelId = &'static str;
+pub type BaseToken = &'static str;
+
+pub type CanBeUsedAsGas = bool;
+pub type Gas = token::Amount;
+pub type MinimumGasPrice = Option<Gas>;
+
+const GAS_TOKENS: [(ChannelId, BaseToken, MinimumGasPrice); 1] = [(
+    "channel-28",
+    "uatom",
+    Some(Gas::from_u64(1)), // 1 uusdc / gas unit
+)];
+
+#[transaction]
+fn apply_tx(ctx: &mut Ctx, _tx_data: BatchedTx) -> TxResult {
+    // Read the current gas cost map
+    let gas_cost_key = get_gas_cost_key();
+    let mut minimum_gas_price: BTreeMap<Address, token::Amount> =
+        ctx.read(&gas_cost_key)?.unwrap_or_default();
+
+    // Set gas cost for each token
+    for (channel_id, base_token, can_be_used_as_gas) in GAS_TOKENS {
+        let ibc_denom = format!("transfer/{channel_id}/{base_token}");
+        let token_address = ibc::ibc_token(&ibc_denom).clone();
+
+        if let Some(gas) = can_be_used_as_gas {
+            minimum_gas_price.insert(token_address.clone(), gas);
+        }
+    }
+
+    // Write the gas cost map back to storage
+    ctx.write(&gas_cost_key, minimum_gas_price)?;
+
+    Ok(())
+}
