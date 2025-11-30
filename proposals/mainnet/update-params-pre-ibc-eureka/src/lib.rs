6dd327f4b95154aba44b595798ec0e04ce1d8ea6
brentstone <brentstone01@gmail.com>
2025-11-30 16:23:48 -0500

    update params needed in order to update ibc eureka wasm tx

---
+++ proposals/mainnet/update-params-pre-ibc-eureka/src/lib.rs
@@ -0,0 +1,13 @@
+use namada_tx_prelude::*;
+use namada_tx_prelude_01502::{self as namada_tx_prelude};
+
+#[transaction]
+fn apply_tx(ctx: &mut Ctx, _tx_data: BatchedTx) -> TxResult {
+    let max_tx_bytes_key = parameters_storage::get_max_tx_bytes_key();
+    ctx.write(&max_tx_bytes_key, 2_000_000_u32)?;
+
+    let max_proposal_code_size_key = gov_storage::keys::get_max_proposal_code_size_key();
+    ctx.write(&max_proposal_code_size_key, 2_000_000_u64)?;
+
+    Ok(())
+}
