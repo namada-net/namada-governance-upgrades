1e91338f28073696a944612f0f82555285493d4b
brentstone <brentstone01@gmail.com>
2025-12-01 13:42:58 -0500

    test

--- proposals/mainnet/update-params-pre-ibc-eureka/src/lib.rs
+++ proposals/mainnet/update-params-pre-ibc-eureka/src/lib.rs
@@ -3,11 +3,8 @@ use namada_tx_prelude_01502::{self as namada_tx_prelude};
 
 #[transaction]
 fn apply_tx(ctx: &mut Ctx, _tx_data: BatchedTx) -> TxResult {
-    let max_tx_bytes_key = parameters_storage::get_max_tx_bytes_key();
-    ctx.write(&max_tx_bytes_key, 2_000_000_u32)?;
-
     let max_proposal_code_size_key = gov_storage::keys::get_max_proposal_code_size_key();
-    ctx.write(&max_proposal_code_size_key, 2_000_000_u64)?;
+    ctx.write(&max_proposal_code_size_key, 3_000_000_u64)?;
 
     Ok(())
 }
