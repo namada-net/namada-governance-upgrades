5c8e41f6ff12310853663de5b3948ff7bdaff97c
Tiago Carvalho <sugoiuguu@tfwno.gf>
2026-08-05 13:48:56 +0200

    Maintain old gas prices

--- proposals/mainnet/disable-ibc-chans-masp-migration/src/lib.rs
+++ proposals/mainnet/disable-ibc-chans-masp-migration/src/lib.rs
@@ -38,11 +38,14 @@ fn apply_tx(ctx: &mut Ctx, _tx_data: BatchedTx) -> TxResult {
 
         disable_old_ibc_rate_limits(ctx, &old_token)?;
         disable_old_masp_inflation(ctx, &old_token)?;
-        remove_old_gas_token(&mut gas_cost, &old_token);
+        let old_gas_price = remove_old_gas_token(&mut gas_cost, &old_token);
         remove_old_token_from_map(&mut token_map, &old_denom);
 
         enable_new_ibc_rate_limits(ctx, &new_token, *mint_limit, *throughput_limit)?;
         enable_new_masp_reward_state(ctx, &new_token)?;
+        if let Some(gas_price) = old_gas_price {
+            gas_cost.insert(new_token.clone(), gas_price);
+        }
         add_new_token_to_map(&mut token_map, new_denom, new_token);
     }
 
@@ -87,8 +90,8 @@ fn disable_old_masp_inflation(ctx: &mut Ctx, old_token: &Address) -> TxResult {
 fn remove_old_gas_token(
     gas_cost: &mut BTreeMap<Address, token::Amount>,
     old_token: &Address,
-) {
-    gas_cost.remove(old_token);
+) -> Option<token::Amount> {
+    gas_cost.remove(old_token)
 }
 
 #[inline(always)]
